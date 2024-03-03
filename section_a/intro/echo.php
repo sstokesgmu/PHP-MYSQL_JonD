be5f33bfa30d7ec85d6d42bd91639cf7e32e6a84
sterlingjstokes <sterlingjstokes@gmail.com>
2024-03-03 12:51:17 +0000

    Adding breaks to the HTML paragraphs

--- section_a/intro/echo.php
+++ section_a/intro/echo.php
@@ -8,7 +8,7 @@
     <h1>The Candy Store</h1>
     <h2><?php echo 'Sterlings\'s'; ?> page</h2>
     <?php echo '<p class="offer">Offer: 20% off</p>' ?>
-    <?php echo '<a href="http://www.google.com">Google</a>'; ?>
-    <?php echo '<a href="http://notes.re/php">PHP</a>'; ?>
+    <?php echo '<a href="http://www.google.com">Google</a><br>'; ?>
+    <?php echo '<a href="http://notes.re/php">PHP</a><br>'; ?>
    </body>
 </html>
