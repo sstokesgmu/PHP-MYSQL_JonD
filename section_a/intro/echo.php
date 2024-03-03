5c5e1ef8ff85232aa0191645e127a00de7342d10
sterlingjstokes <sterlingjstokes@gmail.com>
2024-03-03 12:46:36 +0000

    echo.php another quick check

--- section_a/intro/echo.php
+++ section_a/intro/echo.php
@@ -8,7 +8,7 @@
     <h1>The Candy Store</h1>
     <h2><?php echo 'Sterlings\'s'; ?> page</h2>
     <?php echo '<p class="offer">Offer: 20% off</p>' ?>
-    <?php echo "a herf=\"http://www.google.com\" Google</a>" ?>;
-    <?php echo 'a herf="http://notes.re/php">PHP</a>' ?>;
+    <?php echo '<a href="http://www.google.com">Google</a>'; ?>
+    <?php echo '<a href="http://notes.re/php">PHP</a>'; ?>
    </body>
 </html>
