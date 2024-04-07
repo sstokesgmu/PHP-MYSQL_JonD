788b306b224ce320346ecb11ab0fd62b2f603b80
Sterling Stokes <98550531+sstokesgmu@users.noreply.github.com>
2024-04-06 23:04:33 -0400

    Iphone IOS push
    Hi this is the test of textastic and working copy

--- section_b/c05/array-functions.php
+++ section_b/c05/array-functions.php
@@ -1,6 +1,6 @@
 <?php
 // Create array of greetings then get random value
-$greetings    = ['Hi ', 'Howdy ', 'Hello ', 'Hola ',
+$greetings    = ['Hi', 'Howdy ', 'Hello ', 'Hola ',
                  'Welcome ', 'Ciao ',];
 $greeting_key = array_rand($greetings);
 $greeting     = $greetings[$greeting_key];
@@ -21,10 +21,8 @@ if (array_key_exists('forename', $customer)) {
 }
 ?>
 <?php include 'includes/header.php'; ?>
-
   <h1>Best Sellers</h1>
   <p><?= $greeting ?></p>
   <p>Our top <?= $bestseller_count ?> items today are:
     <b><?= $bestseller_text ?></b></p>
-
 <?php include 'includes/footer.php'; ?>
